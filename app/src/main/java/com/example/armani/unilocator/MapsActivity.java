d7b490dfd61d52f1cb77431e7193edf501e1eb08
1514945 <Armani@Armanis-MBP-2.home>
2017-02-04 15:55:07 +0000

    www

--- app/src/main/java/com/example/armani/unilocator/MapsActivity.java
+++ app/src/main/java/com/example/armani/unilocator/MapsActivity.java
@@ -120,7 +120,7 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.On
         Log.v("DONKEY, "," Starting locations services Called"); //For debugging
 
         try { //See if this works
-            LocationRequest req = LocationRequest.create().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            LocationRequest req = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
              /*PARAMETERS EXPLAINED
             1)It wants the Api Client were using
             2)The request we declared above ( how much power etc)
